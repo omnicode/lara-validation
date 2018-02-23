9416fbc993bfc33b4504ebc5002fd57852ffc615
davitbek <davitzeynalyan1991@gmail.com>
2018-02-24 03:10:44 +0400

    s

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,8 @@
 
 namespace Tests;
 
-use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Orchestra\Testbench\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
 }
